3a3f643c5af55cd7e16b199c3f5f5cd89395993a
nackierun <srisuwan2538@gmail.com>
2019-05-15 17:22:46 +0700

    add status

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -10,26 +10,34 @@ class CustomersController extends Controller
     //
     public function list(){
 
-    $customers = Customer::all();
+    //$customers = Customer::all();
 
-
+        $activecustomers = Customer::where('active',1)->get();
+        $inactivecustomers = Customer::where('active',0)->get();
     //dd($customers);
 
-    return view('customers.customers',[
-        'customers'=>$customers
-    ]);
-    }
+    return view('customers.customers', compact('activecustomers', 'inactivecustomers')
+   // ['activecustomers' => $activecustomers,
+   //     'inactivecustomers' => $inactivecustomers,]
+               );}
     public function store(){
+
+
+
+        //dd($activecustomers);
+        //dd($inactivecustomers);
 
         $data = request()->validate([
             'name' => 'required|min:3',
-            'email' => 'required|email'
+            'email' => 'required|email',
+            'active' => 'required'
         ]);
 
 
         $customers = new Customer();
         $customers->name = request('name');
         $customers->email = request('email');
+        $customers->active = request('active');
         $customers->save();
         return back();
 
--- resources/views/customers/customers.blade.php
+++ resources/views/customers/customers.blade.php
@@ -3,25 +3,69 @@
 Customers
 @endsection
 @section('content')
-<h1>Customers</h1>
+<div class="row col-10">
+
+
+<h1 class="pr-5">Customers</h1>
+<div class="row">
+
+
 <form action="customers" method="POST" class="pb-5">
-    <div class="input-group"><p>Name</p>
+    <div class="form-group col-md-6">
+        <p>Name</p>
     <input type="text" name="name" value="{{ old('name') }}">
     </div>
-<div class=" input-group"><p>Email</p><input type="email" name="email" value="{{ old('email') }}"></div>
-    <div><button type="submit" class="btn btn-primary">Submit</button>
-    @csrf</div>
+
+<div class="form-group col-md-6">
+    <p>Email</p>
+    <input type="email" name="email" value="{{ old('email') }}">
+</div>
+
+
+  <div class="form-group col-md-6">
+    <label for="active">Status</label>
+    <select class="form-group" name="active" id="action">
+          <option value="" aria-disabled="true">Select one</option>
+          <option value="1">Active</option>
+          <option value="0">Inactive</option>
+      </select>
+  </div>
+
+    <div>
+        <button type="submit" class="btn btn-primary col-md-6">Submit</button>
+        @csrf
+    </div>
 
 
     {{ $errors->first('name') }}
     {{ $errors->first('email') }}
+    {{ $errors->first('active') }}
 
 </form>
+</div>
+<div class="row col-md-6">
+
+
+<div class="col-6">
 <ul>
-    @foreach ($customers as $customers)
-<li> {{ $customers->name }}  <span class="text-muted">{{ $customers->email }}</span></li>
+    <h2>Active Users</h2>
+    @foreach ($activecustomers as $activecustomers)
+<li> {{ $activecustomers->name }}  <span class="text-muted">{{ $activecustomers->email }}</span></li>
 
     @endforeach
 </ul>
+</div>
+<div class="col-6">
+<ul>
+    <h2>Inactive Users</h2>
+    @foreach ($inactivecustomers as $inactivecustomers)
+<li> {{ $inactivecustomers->name }}  <span class="text-muted">{{ $inactivecustomers->email }}</span></li>
+
+    @endforeach
+</ul>
+</div>
+
+</div>
+</div>
 
 @endsection
